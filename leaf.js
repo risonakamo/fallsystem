1ff8a8a557687222bc2c0b7d3e53c7bf8018817c
risonakamo <hmykanae@gmail.com>
2018-10-25 12:16:44 -0400

    what...

--- leaf.js
+++ leaf.js
@@ -4,13 +4,13 @@ class Leaf extends Snow
     {
         super(two,options);
 
-        this.fallSpeed=0;
-        this.yAccel=-.05;
-        this.yForce=-.008;
-
-        this.yForces=[-.008,.01];
+        this.yForces=[-.005,.01];
         this.yAccels=[-.02,.09]; //negative acceleration, positive acceleration
         this.speedCutoff=[-1,1.9]; //[negative speed before flipping, positive speed before flipping]
+
+        this.fallSpeed=0;
+        this.yAccel=0;
+        this.yForce=this.yForces[1];
 
         this.multiplier=2;
     }
@@ -19,15 +19,26 @@ class Leaf extends Snow
     {
         this.theShape.translation.y+=this.fallSpeed;
         this.fallSpeed+=this.yAccel;
+        this.yAccel+=this.yForce;
+
+        if (this.yAccel>this.yAccels[1])
+        {
+            this.yAccel=this.yAccel[1];
+        }
+
+        else if (this.yAccel<this.yAccels[0])
+        {
+            this.yAccel=this.yAccels[0];
+        }
 
         if (this.fallSpeed<this.speedCutoff[0]*this.multiplier)
         {
-            this.yAccel=this.yAccels[1]*this.multiplier;
+            this.yForce=this.yForces[1];
         }
 
-        if (this.fallSpeed>this.speedCutoff[1]*this.multiplier)
+        else if (this.fallSpeed>this.speedCutoff[1]*this.multiplier)
         {
-            this.yAccel=this.yAccels[0]*this.multiplier;
+            this.yForce=this.yForces[0];
         }
     }
 }
